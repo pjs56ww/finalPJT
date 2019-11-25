b8000165f81b68b833f8fd5849f3322f80fceed3
JaeseongPark <sysh5656@naver.com>
2019-11-25 17:28:01 +0900

    'home/' 페이지 back-end 응답 함수 완성

--- back-end/movies/views.py
+++ back-end/movies/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Movie
+from .serializers import UserSerializer, MovieSerializer
 from rest_framework.permissions import AllowAny
 
 
@@ -13,3 +14,13 @@ def signup(request):
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         return Response('Sucess Create ID')
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def home(request):
+    movies = Movie.objects.all().order_by('-pk')
+    movies = movies[0:10]
+    movies = sorted(movies, key=lambda movie: -movie.score)
+    serializer = MovieSerializer(many=True, instance=movies)
+    return Response(serializer.data)
