e5b10b2a34f53efd6d97fc5cf844f92dbd943f3f
JaeseongPark <sysh5656@naver.com>
2019-11-27 18:43:33 +0900

    Welcome page 코드 갱신

---
+++ front-end/src/components/WelcomeImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mx-auto">
+    <img :src="bgImage" alt="" class="mx-auto">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeImage',
+  props: {
+    bgImage: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  updated: {
+  }
+}
+</script>
+
+<style>
+
+</style>
--- front-end/src/views/Welcome.vue
+++ front-end/src/views/Welcome.vue
@@ -1,12 +1,77 @@
 <template>
   <div>
-    
+    <WelcomeImage :bgImage="bgImage" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import router from '@/router'
+import WelcomeImage from '@/components/WelcomeImage.vue'
+
+import { mapGetters } from 'vuex'
+const getRandomInt = function(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
 export default {
   name: 'Welcome',
+  components: {
+    WelcomeImage,
+  },
+  computed: {
+    ...mapGetters([
+      'isLoggedIn',
+      'options',
+      'userId'
+    ]),
+    
+    
+  },
+  data() {
+    return {
+      movies: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+      bgImage:''
+    }
+  },
+  methods: {
+    // 사용자 로그인 유무를 확인하여 로그인 되어있을 시 (/home)으로 보내겠다.
+    getMovie() {
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP
+      axios.get(`${SERVER_IP}/api/v1/home/`)
+        .then(response => {
+          // console.log(response)
+          this.movies = response.data
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+    putBackground() {
+      return this.movies[getRandomInt(0, 10)].backgroundImage 
+    },
+    
+    checkLoggedIn() {
+      if (this.isLoggedIn) {
+        console.log(this.isLoggedIn)
+        router.push('/home')
+      }
+    },
+    
+  },
+  created() {
+    this.$nextTick(function () {
+      this.getMovie()
+    })
+    this.bgImage = this.putBackground()
+  },
+  watch: {
+    isLoggedIn() {
+      this.checkLoggedIn()
+      this.getMovie()
+    },
+  }
 }
 </script>
 
